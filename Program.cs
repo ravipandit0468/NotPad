86d1ef7145b662e4e434020a52e0181a361a19f3
RAVI PANDIT <156677726+ravipandit0468@users.noreply.github.com>
2024-12-16 19:55:43 +0530

    Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ADODotNetAppn
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
